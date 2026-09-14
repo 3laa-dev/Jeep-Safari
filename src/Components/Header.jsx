ede07c8a8676d97d059f0418a154dd18a92f7bc7
Alaaelden <alaahhee05@gmail.com>
2026-09-14 18:50:52 +0300

    time line updated

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Col , Row } from 'react-bootstrap';
+
 
 function NavBar() {
   return (
@@ -18,23 +20,30 @@ function NavBar() {
         />
 
         <Navbar.Collapse id="navbarScroll">
-          <div className='d-flex justify-content-between w-100'>
-          <Nav className="navbar-links">
+          <Row className='d-flex justify-content-between w-100'>
+          
+          <Col sm="8" xs="12" >
+              <Form.Control
+              type="search"
+              placeholder="Search..."
+              aria-label="Search"
+              className="w-100 justify-self-center input-search"
+              
+            />
+            </Col>
+
+          <Col sm="4" xs="12">
+            <Nav className="navbar-links">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#home">FAQ</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
           </Nav>
+          </Col>
 
           
-            <Form.Control
-              type="search"
-              placeholder="Search..."
-              aria-label="Search"
-              className="w-50 justify-self-center input-search"
-              
-            />
-          </div>
+            
+          </Row>
           
         </Navbar.Collapse>
       </Container>
