6c1ec66ed7b7da765a8237327a0dfa5e81cd4fe0
Alaaelden <alaahhee05@gmail.com>
2026-09-14 21:20:42 +0300

    QA Created

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -2,8 +2,8 @@ import Accordion from 'react-bootstrap/Accordion';
 
 function QA() {
     return (
-        <div className='m-2'>
-            <div className='my-3 text-qa '>
+        <div className=''>
+            <div className='py-3 text-qa '>
                 <h3>
                     Frequently Asked Questions
                 </h3>
